60745f670cace291459af11c03ecabe9fac7a562
KentoTerakado <kento.terakado@tkk.tokyu.co.jp>
2025-04-02 11:47:54 +0900

    fix

--- terraform/modules/facts/main.tf
+++ terraform/modules/facts/main.tf
@@ -1,5 +1,5 @@
 resource "ansible_group" "proxmox" {
-  name = "all"
+  name = "gather_facts"
 
   variables = {
     ansible_become        = "yes"
@@ -11,34 +11,21 @@ resource "ansible_group" "proxmox" {
 
 resource "ansible_host" "pve_node1" {
   name   = "pve01"
-  groups = ["all"]
+  groups = ["gather_facts"]
 
   variables = {
-    ansible_host                  = "192.168.0.10"
+    ansible_host                  = "192.168.1.13"
     ansible_user                  = "root"
     ansible_ssh_private_key_file = "./id_rsa"
     ansible_python_interpreter   = "/usr/bin/python3"
   }
 }
 
-resource "ansible_host" "pve_node2" {
-  name   = "pve02"
-  groups = ["all"]
-
-  variables = {
-    ansible_host                  = "192.168.0.11"
-    ansible_user                  = "root"
-    ansible_ssh_private_key_file = "./id_rsa"
-    ansible_python_interpreter   = "/usr/bin/python3"
-  }
-}
-
-resource "ansible_playbook" "gather_facts" {
-  name     = "gather_facts"
-  playbook  = "${path.module}/../ansible/playbooks/gather_facts.yml"
-
-  depends_on = [
-    ansible_host.pve_node1,
-    ansible_host.pve_node2
-  ]
-}
+# resource "ansible_playbook" "gather_facts" {
+#   name     = "gather_facts"
+#   playbook  = "${path.module}/../ansible/playbooks/gather_facts.yml"
+# 
+#   depends_on = [
+#     ansible_host.pve_node1,
+#   ]
+# }
--- terraform/services/main.tf
+++ terraform/services/main.tf
@@ -1,11 +1,10 @@
 
-module "proxmox" {
-    source = "../modules/proxmox"
-    targets = local.hosts.proxmox   
-}
-
+# module "proxmox" {
+#     source = "../modules/proxmox"
+#     targets = local.hosts.proxmox   
+# }
+# 
 module "facts" {
     source = "../modules/facts" 
     targets = local.hosts.proxmox
-    depends_on = [ module.proxmox ]
 }
