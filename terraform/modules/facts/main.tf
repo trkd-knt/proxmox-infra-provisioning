a9db3a6feecf76d5a24f7f939a2c0f9bee12f22e
KentoTerakado <kento.terakado@tkk.tokyu.co.jp>
2025-04-02 11:59:13 +0900

    fix

--- terraform/modules/facts/main.tf
+++ terraform/modules/facts/main.tf
@@ -22,7 +22,7 @@ resource "ansible_host" "pve_node1" {
 }
 
 resource "ansible_playbook" "gather_facts" {
-  name     = "gather_facts"
+  name     = "playbook_gather_facts"
   playbook  = "${path.module}/ansible/playbooks/gather_facts.yml"
 
   depends_on = [
