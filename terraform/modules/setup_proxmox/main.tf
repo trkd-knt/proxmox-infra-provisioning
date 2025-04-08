ad79939d2069dd0e2c5bec09b96b1b5ace93192f
KentoTerakado <kento.terakado@tkk.tokyu.co.jp>
2025-04-08 14:57:12 +0900

    fix

--- terraform/modules/setup_proxmox/main.tf
+++ terraform/modules/setup_proxmox/main.tf
@@ -28,15 +28,15 @@ resource "ansible_playbook" "setup_all_host" {
   replayable = false
 
   extra_vars = {
-    network.uplink_interface = each.value.uplink_interface
-    network.mgmt.address = each.value.manageip
-    network.mgmt.gateway = each.value.gatewayip
-    network.vlan10.address = each.value.vlan10.address
-    network.vlan10.gateway = each.value.vlan10.gateway
-    network.vlan20.address = each.value.vlan20.address
-    network.vlan20.gateway = each.value.vlan20.gateway
-    network.vlan30.address = each.value.vlan30.address
-    network.vlan30.gateway = each.value.vlan30.gateway
+    uplink_interface = each.value.uplink_interface
+    mgmt_address = each.value.manageip
+    mgmt_gateway = each.value.gatewayip
+    vlan10_address = each.value.vlan10.address
+    vlan10_gateway = each.value.vlan10.gateway
+    vlan20_address = each.value.vlan20.address
+    vlan20_gateway = each.value.vlan20.gateway
+    vlan30_address = each.value.vlan30.address
+    vlan30_gateway = each.value.vlan30.gateway
     ntp_servers = join(" ", each.value.ntp_servers)
   }
 
